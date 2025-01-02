4cb5413598415c39728f3262854e0cf670f460dc
VasuModi_01 <vasumodi2020@gmail.com>
2025-01-02 22:02:51 +0530

    Line No. 7 Changed

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,8 @@ const app = express();
 require("dotenv").config();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 async function connectDatabase() {
@@ -16,12 +18,10 @@ async function connectDatabase() {
   }
 }
 
-async function connect()
-{
-   await connectDatabase();
+async function connect() {
+  await connectDatabase();
 }
 connect();
-
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
