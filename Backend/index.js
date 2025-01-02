cc3383f9bcfe20f445613923a722339fdb819253
Rishee3105 <risheegandhi3105@gmail.com>
2025-01-02 19:40:47 +0530

    setUpDb

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,27 @@
+const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const prisma = new PrismaClient();
 
 const PORT = process.env.PORT || 3000;
+
+async function connectDatabase() {
+  try {
+    await prisma.$connect();
+    console.log("Db connected.");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+async function connect()
+{
+   await connectDatabase();
+}
+connect();
+
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
