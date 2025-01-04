b234c16ab07e0f5a245ac6dbf0a4a496c3b94a08
Rishee3105 <risheegandhi3105@gmail.com>
2025-01-04 11:54:49 +0530

    .

---
+++ Backend/prisma/migrations/20250104061850_user_model/migration.sql
@@ -0,0 +1,101 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('STUDENT', 'TEACHER');
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL,
+    "collegeId" TEXT NOT NULL,
+    "institute" TEXT NOT NULL,
+    "department" TEXT NOT NULL,
+    "domain" TEXT,
+    "aboutMe" TEXT,
+    "currCgpa" DOUBLE PRECISION,
+    "phoneNumber" TEXT,
+    "profilePhoto" TEXT,
+    "achievements" TEXT[],
+    "socialLinks" TEXT[],
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserSkill" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "skill" TEXT NOT NULL,
+
+    CONSTRAINT "UserSkill_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Experience" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "duration" TEXT NOT NULL,
+
+    CONSTRAINT "Experience_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "link" TEXT,
+    "details" TEXT,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Certificate" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "url" TEXT,
+
+    CONSTRAINT "Certificate_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_collegeId_key" ON "users"("collegeId");
+
+-- CreateIndex
+CREATE INDEX "users_firstName_lastName_idx" ON "users"("firstName", "lastName");
+
+-- CreateIndex
+CREATE INDEX "users_email_domain_role_idx" ON "users"("email", "domain", "role");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserSkill_userId_skill_key" ON "UserSkill"("userId", "skill");
+
+-- AddForeignKey
+ALTER TABLE "UserSkill" ADD CONSTRAINT "UserSkill_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Experience" ADD CONSTRAINT "Experience_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Certificate" ADD CONSTRAINT "Certificate_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
